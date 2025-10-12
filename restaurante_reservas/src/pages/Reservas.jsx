6fd2053d015057706dd201692823803dae6e8271
leobragaa <leombraga11@gmail.com>
2025-10-11 21:28:25 -0400

    Criando Padronização de Arquivos

---
+++ restaurante_reservas/src/pages/Reservas.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import {useNavigate} from 'react-router-dom';
+import axios from 'axios';
+
+export default function Reservas() {
+    return(
+        <div className='reservas-container'>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
